706f8b21ebce203991c74e7cc6a855d249148259
Emory Dunn <emdunn@google.com>
2014-02-25 15:32:29 -0800

    Initial commit. PyTimer script to import and TimingTest companion script.

---
+++ PyTimer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+
+try:
+    import time
+except ImportError:
+    print ("Time not found. ")
+    quit()
+
+class Timer():
+    def __init__(self, unit):
+        unit = str(unit)
+        self.accepted = False      
+        try:
+            if unit == 's':
+                print unit
+                self.timeoutMultiplier = 1
+                pass
+            elif unit == 'm':
+                print unit
+                self.timeoutMultiplier = 60
+                pass
+            elif unit == 'h':
+                print unit
+                self.timeoutMultiplier = 3600
+                pass
+            else:
+                raise
+        except:
+            print ("Please enter \'s\', \'m\', or \'h\'. ")
+            quit()
+        
+    def start(self, begin, timeout):
+        """Inizialize the timer"""
+        timeout = timeout * self.timeoutMultiplier
+        self.end = begin + timeout
+        #print self.end
+        #print (round(int(time.time()), 10))
+    pass
+    
+    def test(self):
+        """Query the timer"""
+        if time.time() >= self.end:
+            self.accepted = True
+        else:
+            self.accepted = False
+    pass
---
+++ TimingTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys, time
+from PyTimer import Timer
+
+duration = input("How long? ")
+unit = raw_input("Unit? ")
+startTime = (round(int(time.time()), 10))
+
+clock = Timer(unit)
+clock.start(startTime, duration)
+
+while clock.accepted == False:
+    sys.stdout.write ("Not yet\r")
+    clock.test()
+else:
+    sys.stdout.write ("Ding        \n")
